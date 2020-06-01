4bef5d257ec3ee45a71dcdd1b1b321da6af357f5
ranronghua <ranronghua@bytedance.com>
2020-06-01 23:37:14 +0800

    关闭首页

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5d102e443968e161f26";
+/******/ 	var hotCurrentHash = "af8c3c66be84f5a4bf12";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/webpack/styles.fd04489fb5100c95dd0f.hot-update.js
+++
@@ -1,9 +0,0 @@
-webpackHotUpdate("styles",{
-
-/***/ "./styles/index.scss":
-false,
-
-/***/ 4:
-false
-
-})
