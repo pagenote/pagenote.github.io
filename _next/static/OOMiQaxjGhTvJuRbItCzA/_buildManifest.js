89be126de702352270b4b1d34e70c4fac37bf2f1
ranronghua <ranronghua@bytedance.com>
2020-06-03 21:55:03 +0800

    开放广场

---
+++ _next/static/OOMiQaxjGhTvJuRbItCzA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/snapshot":[a,b,c]}}("static\u002Fchunks\u002F3e9627c1beba73300a72f803dc48aeb9ec9d6bc9.35c61ec26a10d9bef3b1.js","static\u002Fcss\u002Fstyles.f08b5372.chunk.css","static\u002Fchunks\u002Fstyles.bb3fe6d2c95feb7683ef.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
