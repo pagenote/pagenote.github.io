a7ba45bb441c5af876f3cabac5fc528b860d8665
ranronghua <ranronghua@bytedance.com>
2020-06-03 21:57:34 +0800

    开放

---
+++ _next/static/Vz2G3PmV_PMAXyRgTIfcR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/snapshot":[a,b,c]}}("static\u002Fchunks\u002Fdf8319dc7d900d4e14755eb619cd647fe275a7d7.35c61ec26a10d9bef3b1.js","static\u002Fcss\u002Fstyles.f08b5372.chunk.css","static\u002Fchunks\u002Fstyles.bb3fe6d2c95feb7683ef.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
