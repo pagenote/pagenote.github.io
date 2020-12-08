f5d08262bee155531b79c39c6a04653074bdad7d
rowthan <ranronghuano1@163.com>
2020-12-08 22:37:16 +0800

    版本比较fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,15 @@
-export function isLow(current,compareVersion,splitor='.') {
-    if(!current){
-        return true;
-    }
+export function isLow(current='',compareVersion) {
     if(current===compareVersion){
         return false;
     }
     const firstVersion = current.split('.');
     const secondVersion = compareVersion.split('.');
-    let isOld = false;
+    let isOld = true;
     for(let i=0; i<secondVersion.length; i++) {
         const preVersion = parseInt(firstVersion[i]);
         const nexVersion = parseInt(secondVersion[i]);
-        if(preVersion<nexVersion){
-            isOld = true;
+        if(preVersion!==nexVersion){
+            isOld = preVersion<nexVersion;
             break;
         }
     }
