0574b24b2787b8fac2fbb2f9abb18b3826d46e3d
rowthan <ranronghuano1@163.com>
2020-12-17 22:59:34 +0800

    draft 增加更新日期展示

--- src/pages/draft/index.js
+++ src/pages/draft/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Editor from '../../components/editor/Editor'
 import '../page/page.scss'
 // import CommonPage from "../CommonPage";
+import ExportIcon from '@/assets/icon/export.svg'
+import ImportIcon from '@/assets/icon/import.svg'
 import './new.scss'
 
 
@@ -23,6 +25,19 @@ export default class CreatePage extends Component{
     return (
       <div className='page' data-pagenote='new'>
         <div className='editor_container'>
+          <div className='fun-icons'>
+            <div>
+              <span>
+              {
+                new Date(data.time).toLocaleString()
+              }
+            </span>
+            </div>
+            {/*<div>*/}
+            {/*  <ImportIcon width={16} height={16}></ImportIcon>*/}
+            {/*  <ExportIcon width={16} height={16}></ExportIcon>*/}
+            {/*</div>*/}
+          </div>
           <Editor placeholder='在这里记录写什么吧，数据存储在你浏览器本地，你可以把这里当做一个草稿记事本' data={data} onSave={this.saveData}></Editor>
         </div>
       </div>
